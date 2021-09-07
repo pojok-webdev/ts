85d1faaf83d65f4df130b4bf3510711f1e6b14e6
pujie <pw.prayitno@gmail.com>
2021-09-07 14:36:39 +0700

    change baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ftp/';
+$config['base_url'] = 'http://ts.padinet.com/';
 
 /*
 |--------------------------------------------------------------------------
