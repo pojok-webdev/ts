76fb141579eb42006570af4d99ba5f23eda40a53
pujie <pw.prayitno@gmail.com>
2021-09-09 14:18:49 +0700

    add surveys

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,4 +9,53 @@ Class Home extends CI_Controller{
         );
         $this->load->view('pages/home',$data);
     }
+	function getfolders(){
+		$this->load->library('ftp');
+
+		$config['hostname'] = '192.168.0.234';
+		$config['username'] = 'puji';
+		$config['password'] = 'puj12021';
+		$config['debug']    = TRUE;
+
+		$this->ftp->connect($config);
+
+		//$list = $this->ftp->list_files('/Task\ List/NB/Puji\ Widi\ Prayitno');
+		//$list = $this->ftp->list_files('/Task List/NB/Puji Widi Prayitno');
+		echo 'Aplikasi\n';
+		$list = $this->ftp->list_files('/Aplikasi');
+		print_r($list);
+		echo '\n\nTask List\n';
+		$list = $this->ftp->list_files('/Task List/NB/Puji Widi P');
+
+		print_r($list);
+		echo '<br >';
+		echo '<br >';
+		echo '<br >';
+		echo '<br >';
+		echo $list[0];
+		$this->ftp->close();
+
+	}
+	function ftpcreatefolder(){
+		$this->load->library('ftp');
+
+		$config['hostname'] = '192.168.0.234';
+		$config['username'] = 'puji';
+		$config['password'] = 'puj12021';
+		$config['debug']    = TRUE;
+
+		$this->ftp->connect($config);
+		if($this->ftp->mkdir('/Task List/NB/Puji Widi P/kholis',0755)){
+			echo 'sukses membuat Folder';
+		}else{
+			echo 'tidak bisa membuat folder';
+		};
+		$this->ftp->close();
+	}
+	function createfolder(){
+		$target_dir="../../media/profile/".date('my')."/";
+		if(!file_exists($target_dir)){
+			mkdir($target_dir,0777);
+		}
+	}
 }
---
+++ application/views/surveys/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $this->load->view('commons/head')?>
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    <?php $this->load->view('commons/sidebar');?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <form class="form-inline">
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </form>
+
+                    <!-- Topbar Search -->
+                    <form
+                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <?php $this->load->view('commons/userinformation');?>
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Action</h1>
+                    <p class="mb-4">Berikut akan melakukan generate report (PDF) ke  Lavender.</p>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">PElanggan PadiNET</h6>
+                            <button id="btnDownloadSelected">Download Selected</button>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th><input type="checkbox" class="checkAll"></th>
+                                            <th>Name</th>
+                                            <th>Alias</th>
+                                            <th>Address</th>
+                                            <th>Install Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach($clients as $client){?>
+                                        <tr>
+                                            <td><input type="checkbox" class="clients" value='<?php echo $client->installsiteid;?>'></td>
+                                            <td><?php echo $client->name;?></td>
+                                            <td><?php echo $client->alias;?></td>
+                                            <td><?php echo $client->address;?></td>
+                                            <td><?php echo $client->installsdate;?></td>
+                                            <td>
+                                            <button class="btn btn-primary dropdown-toggle" type="button"
+                                                id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                                Action
+                                            </button>
+                                            <div class="dropdown-menu animated--fade-in"
+                                                aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="/installs/createreport/<?php echo $client->installsiteid;?>">Generate Install Report</a>
+                                                <a class="dropdown-item" href="/surveys/createreport/<?php echo $client->id;?>">Generate Survey Report</a>
+                                                <a class="dropdown-item" href="/troubleshoots/createreport/<?php echo $client->id;?>">Generate Troubleshoot Report</a>
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        <?php }?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; PadiApp 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php $this->load->view('commons/footer');?>
+    <script src="/assets/padinet/js/padiFtp.js"></script>
+</body>
+
+</html>
